b024334b4ff37e5edc2391caa0685121b3f94390
mostafa mobasher <mobashermostafa@gmail.com>
2023-10-06 17:04:58 +0330

    add create service into the controller

--- aspnetcore7/Controllers/CategoryController.cs
+++ aspnetcore7/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using aspnetcore7.Services;
+﻿using aspnetcore7.Models;
+using aspnetcore7.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace aspnetcore7.Controllers
@@ -16,6 +17,18 @@ namespace aspnetcore7.Controllers
 		{
 			var list = await Service.GetListAsync();
 			return View(list);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(Category category)
+		{
+			await Service.Create(category);
+			return View(category);
 		}
 	}
 }
