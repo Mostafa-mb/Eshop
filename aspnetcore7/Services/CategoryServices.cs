27cf98e7378c99dabf00fda3cdd6f70833812959
mostafa mobasher <mobashermostafa@gmail.com>
2023-10-06 14:18:14 +0330

    add category services

---
+++ aspnetcore7/Services/CategoryServices.cs
@@ -0,0 +1,29 @@
+﻿using aspnetcore7.Data;
+using aspnetcore7.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetcore7.Services
+{
+	public class CategoryServices
+	{
+		private readonly AppDbContext context;
+
+		public CategoryServices(AppDbContext context)
+        {
+			this.context = context;
+		}
+
+		public async Task<List<Category>> GetList()
+		{
+			return await context.Category.ToListAsync();
+		}
+		
+		public async Task<Category> Create(Category category)
+		{
+			context.Add(category);
+			await context.SaveChangesAsync();
+			return category;
+		}
+
+	}
+}
